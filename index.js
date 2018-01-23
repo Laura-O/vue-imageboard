fecd91fc021660e6644a5d52791df67e8dea2089
Laura-O <laura.ohrndorf@gmail.com>
2018-01-23 11:11:14 +0100

    Add file upload & S3

--- index.js
+++ index.js
@@ -1,20 +1,94 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const db = require("./db.js");
+const db = require('./db.js');
+const config = require('./config');
 
-app.use(express.static("./public"));
+const multer = require('multer');
+const uidSafe = require('uid-safe');
+const path = require('path');
+const fs = require('fs');
 
-app.get("/db", (req, res) => {
-    const query = "SELECT * FROM images";
+const knox = require('knox');
+let secrets;
+if (process.env.NODE_ENV == 'production') {
+    secrets = process.env; // in prod the secrets are environment variables
+} else {
+    secrets = require('./secrets'); // secrets.json is in .gitignore
+}
+const client = knox.createClient({
+    key: secrets.AWS_KEY,
+    secret: secrets.AWS_SECRET,
+    bucket: 'peachan',
+});
+
+var diskStorage = multer.diskStorage({
+    destination: function(req, file, callback) {
+        callback(null, __dirname + '/uploads');
+    },
+    filename: function(req, file, callback) {
+        uidSafe(24).then(function(uid) {
+            callback(null, uid + path.extname(file.originalname));
+        });
+    },
+});
+
+var uploader = multer({
+    storage: diskStorage,
+    limits: {
+        fileSize: 2097152,
+    },
+});
+
+app.use(express.static('./public'));
+
+app.get('/db', (req, res) => {
+    const query = 'SELECT * FROM images';
     db
         .query(query)
         .then(results => {
-            console.log(results);
+            for (var i = 0; i < results.rows.length; i++) {
+                results.rows[i].image = config.s3Url.concat(results.rows[i].image);
+            }
             return res.json(results.rows);
         })
         .catch(err => {
-            console.error("query error", err.message, err.stack);
+            console.error('query error', err.message, err.stack);
         });
+});
+
+app.post('/upload-image', uploader.single('file'), function(req, res) {
+    // If nothing went wrong the file is already in the uploads directory
+    if (req.file) {
+        const { username, title, description } = req.body;
+
+        const s3Request = client.put(req.file.filename, {
+            'Content-Type': req.file.mimetype,
+            'Content-Length': req.file.size,
+            'x-amz-acl': 'public-read',
+        });
+        const readStream = fs.createReadStream(req.file.path);
+        readStream.pipe(s3Request);
+
+        s3Request.on('response', s3Response => {
+            const wasSuccessful = s3Response.statusCode == 200;
+            const query =
+                'INSERT INTO images (image, username, title, description) VALUES ($1, $2, $3, $4)';
+
+            db
+                .query(query, [req.file.filename, username, title, description])
+                .then(() => {
+                    res.json({ success: wasSuccessful });
+                })
+                .catch(err => {
+                    console.log(err);
+                    res.json({ success: false });
+                });
+        });
+    } else {
+        res.json({
+            success: false,
+        });
+    }
 });
 
 app.listen(8080);
--- public/js/script.js
+++ public/js/script.js
@@ -1,14 +1,35 @@
 (function() {
     var app = new Vue({
-        el: "#main",
+        el: '#main',
         data: {
-            domain: "https://s3.amazonaws.com/spicedling/",
-            images: ""
+            images: '',
+            formStuff: {
+                title: '',
+                description: '',
+            },
         },
         mounted: function() {
-            axios.get("/db").then(function(resp) {
+            axios.get('/db').then(function(resp) {
                 app.images = resp.data;
             });
-        }
+        },
+        methods: {
+            chooseFile: function(e) {
+                this.formStuff.file = e.target.files[0];
+            },
+            uploadFile: function(e) {
+                e.preventDefault();
+
+                const formData = new FormData();
+                formData.append('file', this.formStuff.file);
+                formData.append('title', this.formStuff.title);
+                formData.append('description', this.formStuff.description);
+                formData.append('username', this.formStuff.username);
+
+                axios.post('/upload-image', formData).then(response => {
+                    console.log(response);
+                });
+            },
+        },
     });
 })();
